652ce6e7bc83217b73b1496642b3ed30e468f1ef
jyfernandez <jydfernandez@gmail.com>
2021-08-24 20:57:56 +0800

    Handle routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
 app.use("/add-product", (req, res, next) => {
   console.log("Add product middleware");
   res.send("<h1>Add product</h1>");
