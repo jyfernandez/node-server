b01c9d14b42b42b1c8357f34601a3bcb5c8a5f4a
jyfernandez <jydfernandez@gmail.com>
2021-08-25 19:36:59 +0800

    Handle add product

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ app.use("/", (req, res, next) => {
   next();
 });
 app.use("/add-product", (req, res, next) => {
-  console.log("Add product middleware");
-  res.send("<h1>Add product</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>"
+  );
 });
-
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
   res.send("<h1>Hello from Express</h1>");
 });
 
