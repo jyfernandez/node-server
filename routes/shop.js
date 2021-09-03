cfae822daa29e7fa6c9f9ac1d6a3d280f1606381
jyfernandez <jydfernandez@gmail.com>
2021-09-03 19:18:30 +0800

    Display hbs

--- routes/shop.js
+++ routes/shop.js
@@ -5,10 +5,15 @@ const adminData = require("./admin");
 
 const router = express.Router();
 router.get("/", (req, res, next) => {
-  console.log(adminData.products);
   const products = adminData.products;
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
