ce9a24d9e5cc13027d90b4e15dc53c32a715f3e5
jyfernandez <jydfernandez@gmail.com>
2021-09-03 19:17:51 +0800

    Add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const express = require("express");
-
+const expressHbs = require("express-handlebars");
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -15,7 +23,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(8000);
