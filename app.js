2c6e14e40117dcfc1a3ba8090fca9b87e6e2b830
jyfernandez <jydfernandez@gmail.com>
2021-09-04 23:40:28 +0800

    Create controller for error

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -13,8 +15,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(8000);
